7dc62c38f24b96ca6c640f652db71d7c2152e0eb
Gaurav-kangane <163861574+Gaurav-kangane@users.noreply.github.com>
2025-10-30 23:47:31 +0530

    Day-4

--- Day-4-State-Locking/backend.tf
+++ Day-4-State-Locking/backend.tf
@@ -2,7 +2,9 @@ terraform {
   backend "s3" {
         bucket = "kangane"
         key = "day-4/terraform.tfstate"
-        use_lockfile = true
+        #use_lockfile = true         #to use s3 native locking 1.19 version above
         region = "ap-south-1"
+        dynamodb_table = "Gaurav"    # any version we can use dynamobd locking
+        encrypt = true
   }
 }
